846ba39aa4660bf8f88f1067d8ba140e00eaf9f8
Joel <joelclaude0817@gmail.com>
2026-02-15 20:57:17 +0800

    Implement basic command line interface for WaveOS

---
+++ waveos.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    printf("Welcome To WaveOS!\n");
+    char option[100];
+
+    while (1) {
+        printf("waveos> ");
+        fgets(option, sizeof(option), stdin);
+        option[strcspn(option, "\n")] = 0; // Remove newline character
+
+        if (strcmp(option, "help") == 0) {
+            printf("help - Shows this help message\n");
+            printf("wave - Activates the wave\n");
+            printf("info - Shows the information\n");
+            printf("version - Shows the version\n");
+            printf("shutdown - shutdowns the os\n");
+        } else if (strstr(option, "wave") != NULL) {
+            printf("WAVE ACTIVATED🌊\n");
+        } else if (strstr(option, "info") != NULL) {
+            printf("Version: 1.0.0\n");
+            printf("How it's made: With C\n");
+        } else if (strstr(option, "version") != NULL) {
+            printf("1.0.0\n");
+        } else if (strstr(option, "shutdown") != NULL) {
+            printf("Shutting down WaveOS...\n");
+            break;
+        } else {
+            printf("Invalid Command: %s\n", option);
+        }
+    }
+    return 0;
+}
